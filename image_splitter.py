9688a7d1b9cec83c679d1979d26e0151c5e3350d
TheMarioModder <47430203+TheMarioModder@users.noreply.github.com>
2019-11-21 22:00:37 -0800

    Initial release
    Initial release of program

---
+++ image_splitter.py
@@ -0,0 +1,39 @@
+import cv2
+import sys
+
+def splitimage(sidepixels, original_img, returnfilename):
+
+    height = original_img.shape[0]
+    width = original_img.shape[1]
+    extention = ".png"
+    counter = 0
+    
+    images = []
+    for i in range(0,width,sidepixels):
+        row = []
+        for j in range (0,height, sidepixels):
+            temp_img = original_img[j:j + sidepixels, i:i+sidepixels]
+
+            counter = counter + 1
+            newfilename = returnfilename + "_" + str(counter) + extention
+            
+            cv2.imwrite(newfilename,temp_img)
+            
+
+
+def bail(message):
+    print(message)
+    sys.exit()
+
+print ("PNG Splitter v1.0")
+print ("\nInput the file name")
+filename = input("")
+print ("Input the size of the image (square)")
+try:
+    sidepixels = int(input(""))
+except ValueError:
+    bail("Nope, has to be an integer. Restart the program")
+print ('Input result name without ".png" extention')
+returnfilename = input("")
+original_img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+images = splitimage(sidepixels, original_img, returnfilename)
