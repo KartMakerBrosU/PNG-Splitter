141cadb7414c5ad379e344d81a34e9d2130818cc
TheMarioModder <47430203+TheMarioModder@users.noreply.github.com>
2019-11-23 10:23:56 -0800

    Update Program
    Result files named efficiently
    Added "Press enter to continue" in case of crash.

--- image_splitter.py
+++ image_splitter.py
@@ -6,7 +6,7 @@ def splitimage(sidepixels, original_img, returnfilename):
     height = original_img.shape[0]
     width = original_img.shape[1]
     extention = ".png"
-    counter = 0
+    counter = -1
     
     images = []
     for i in range(0,width,sidepixels):
@@ -15,7 +15,7 @@ def splitimage(sidepixels, original_img, returnfilename):
             temp_img = original_img[j:j + sidepixels, i:i+sidepixels]
 
             counter = counter + 1
-            newfilename = returnfilename + "_" + str(counter) + extention
+            newfilename = returnfilename + "." + str(counter) + extention
             
             cv2.imwrite(newfilename,temp_img)
             
@@ -23,9 +23,9 @@ def splitimage(sidepixels, original_img, returnfilename):
 
 def bail(message):
     print(message)
-    sys.exit()
+    input("\nPress Enter to exit")
 
-print ("PNG Splitter v1.0")
+print ("PNG Splitter v1.1")
 print ("\nInput the file name")
 filename = input("")
 print ("Input the size of the image (square)")
@@ -34,6 +34,7 @@ try:
 except ValueError:
     bail("Nope, has to be an integer. Restart the program")
 print ('Input result name without ".png" extention')
+print ('Result name will be "INPUT.0.png')
 returnfilename = input("")
 original_img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
 images = splitimage(sidepixels, original_img, returnfilename)
